944fe5b18fe5c1a743a10fcee975f732522b3345
Agustín Job Hernández Montes <agus_job@hotmail.com>
2022-01-27 00:00:13 -0600

    retiro/deposito abrir turno, cerrar turno

--- assets/js/myjs/panel_inicio.js
+++ assets/js/myjs/panel_inicio.js
@@ -1,8 +1,71 @@
-function almacenarDepositosRetiro() {
+function abrirTurno() {
+    var idTurno = localStorage.getItem('idTurno');
 
-    $("#tipoRetiro").val();
-    $("#conceptoRetiro").val();
-    $("#referenciaRetiro").val();
-    $("#importeRetiro").val();
+    if (idTurno == null) {
+        var fecha = new Date();
+        var idUsuariotxt = localStorage.getItem('idUsuario');
+        var fondoInicialtxt = $("#turnoFondoInicial").val();
+        $.ajax({
+            url: "http://localhost:8082/v1/turnos",
+            type: "POST",
+            data: JSON.stringify({
+                fechaApertura: fecha,
+                fechaCierre: fecha,
+                fondoInicial: fondoInicialtxt,
+                usuario: idUsuariotxt,
+                estatus: "abierto",
+                turno: 1,
+                efectivoDeclarado: 0
+            }),
+            contentType: 'application/json; charset=utf-8',
+            success: function(data) {
+                localStorage.setItem("idTurno", data.idTurno);
+                alert('Registro agregado exitosamente !!!');
+                $("#turnoFondoInicial").val("");
+            },
+            failure: function(data) {
+                alert(data.responseText);
+            },
+            error: function(data) {
+                alert(data.responseText);
+            }
+        });
+    } else {
 
+        alert("Ya hay un turno abierto, no puedes tener dos turnos abiertos a la vez");
+    }
+}
+
+function cerrarTurno() {
+    var idTurnotxt = localStorage.getItem('idTurno');
+
+    if (idTurnotxt != null) {
+        var fecha = new Date();
+        var fondoDeclaradotxt = $("#turnoFondoDeclarado").val();
+        $.ajax({
+            url: "http://localhost:8082/v1/turnos",
+            type: "PUT",
+            data: JSON.stringify({
+                idTurno: idTurnotxt,
+                fechaCierre: fecha,
+                efectivoDeclarado: fondoDeclaradotxt
+            }),
+            contentType: 'application/json; charset=utf-8',
+            success: function(data) {
+                localStorage.removeItem("idTurno");
+                $("#turnoFondoDeclarado").val("");
+                alert('Registro agregado exitosamente !!!');
+            },
+            failure: function(data) {
+                alert(data.responseText);
+            },
+            error: function(data) {
+                alert(data.responseText);
+            }
+        });
+    } else {
+
+        alert("No puedes cerrar si no haz abierto ningún turno");
+        $("#turnoFondoDeclarado").val("");
+    }
 }
--- assets/js/myjs/utilidades.js
+++ assets/js/myjs/utilidades.js
@@ -1,5 +1,4 @@
 function revisarSesion() {
-    console.log("ingreso a revisar sesión")
     var user = localStorage.getItem("user");
     var pass = localStorage.getItem("password");
 
