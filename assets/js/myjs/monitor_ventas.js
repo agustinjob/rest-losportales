fc4d03a880b21edd9d5fdb02f3c037f21ed51df0
Agustín Job Hernández Montes <agus_job@hotmail.com>
2022-02-03 23:47:53 -0600

    monitor ventas terminado

--- assets/js/myjs/monitor_ventas.js
+++ assets/js/myjs/monitor_ventas.js
@@ -1,21 +1,32 @@
 function probar() {
     //new Date().getTime() - (new Date().getTimezoneOffset() * 60000)
     var fechatxt = formatoFecha(new Date(), "yy/mm/dd");
-    console.log(fechatxt);
+    var txtTipoTurno = $("#tipoTurno").val();
+    var idTurnotxt = localStorage.getItem("idTurno");
+    var estatustxt = $("#tipoVenta").val();
 
     $.ajax({
         url: "http://localhost:8082/v1/cuentas-ventas",
         type: "POST",
         data: JSON.stringify({
-            tipoTurno: "todos",
-            idTurno: "61fa08864297c65737d3b5e0",
-            estatus: "abierta",
+            tipoTurno: txtTipoTurno,
+            idTurno: idTurnotxt,
+            estatus: estatustxt,
             fecha: fechatxt
-
         }),
         contentType: 'application/json; charset=utf-8',
         success: function(data) {
-            alert('Registro agregado exitosamente !!!');
+            $("#toBebidas").val(data.bebidas);
+            $("#toAlimentos").val(data.alimentos);
+            $("#toOtros").val(data.otros);
+            $("#ventaTotal").val(data.ventaTotal);
+            $("#impuestos").val(data.impuestos);
+            $("#descuentos").val(data.descuentos);
+            $("#cuenCerradas").val(data.cuentasPagadas);
+            $("#cuenAbiertas").val(data.cuentasAbiertas);
+            $("#porBebidas").text(data.porcentajeBebidas + " %");
+            $("#porAlimentos").text(data.porcentajeAlimentos + " %");
+            $("#porOtros").text(data.procentajeOtros + " %");
         },
         failure: function(data) {
             alert(data.responseText);
@@ -37,4 +48,9 @@ function formatoFecha(fecha, formato) {
     }
     return fecha.getFullYear() + "-" + txt + "-" + txt2;
 
+}
+
+function inicializarDatos() {
+    var fechatxt = formatoFecha(new Date(), "yy/mm/dd");
+    $("#fechaDia").val(fechatxt);
 }
